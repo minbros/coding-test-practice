c00616b00d3402b67be33a230ea1a4630cba5616
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-01-11 21:43:29 +0900

    [level 2] Title: 전화번호 목록, Time: 461.61 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,13 @@
-def solution(strings):
-    strings.sort()
-    
-    for i in range(len(strings) - 1):
-        if strings[i + 1].startswith(strings[i]):
-            return False
-    
-    return True
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
+
