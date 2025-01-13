29b6813fa5f90cb7f66ef4f4fafbd2e5914a4b29
minbros <pristo24631@gmail.com>
2025-01-13 13:39:25 +0900

    Stack/Queue - 2번 문제 완료

---
+++ programmers/stack-queue/2.py
@@ -0,0 +1,24 @@
+# 기능개발
+# https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+def solution(progresses, speeds):
+    import math
+    from collections import deque
+
+    days = deque()
+    for p, s in zip(progresses, speeds):
+        d = math.ceil((100 - p) / s)
+        days.append(d)
+
+    result = []
+    while len(days) != 0:
+        c = 1
+        tmp = days.popleft()
+
+        while len(days) != 0 and days[0] <= tmp:
+            days.popleft()
+            c += 1
+
+        result.append(c)
+
+    return result
