1df3fec040ebca109d21602d2af7ceabe3cd0f7a
minbros <pristo24631@gmail.com>
2025-02-05 23:09:06 +0900

    Select - 5개 완료

---
+++ programmers-sql/select/133025.sql
@@ -0,0 +1,11 @@
+-- 과일로 만든 아이스크림 고르기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133025
+
+SELECT FLAVOR
+FROM FIRST_HALF
+WHERE TOTAL_ORDER > 3000 AND FLAVOR IN (
+    SELECT FLAVOR
+    FROM ICECREAM_INFO
+    WHERE INGREDIENT_TYPE = 'FRUIT_BASED'
+)
+ORDER BY TOTAL_ORDER DESC
