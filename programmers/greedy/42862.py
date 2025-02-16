13b1ec3f1069ae4a03725c83ae3ff1efe3818b97
minbros <pristo24631@gmail.com>
2025-02-16 23:56:35 +0900

    Greedy - 42862번 완료

---
+++ programmers/greedy/42862.py
@@ -0,0 +1,24 @@
+# 체육복
+# https://school.programmers.co.kr/learn/courses/30/lessons/42862
+
+def solution(n, lost, reserve):
+    e_lost = []
+    for l in lost:
+        if l in reserve:
+            reserve.remove(l)
+        else:
+            e_lost.append(l)
+
+    result = n - len(e_lost)
+    e_lost.sort()
+    reserve.sort()
+
+    for l in e_lost:
+        if l - 1 in reserve:
+            reserve.remove(l - 1)
+            result += 1
+        elif l + 1 in reserve:
+            reserve.remove(l + 1)
+            result += 1
+
+    return result
