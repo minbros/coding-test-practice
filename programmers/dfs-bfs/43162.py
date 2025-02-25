5d81b6b5c36608e58b9cfb3d4d1f01420e7b904a
minbros <pristo24631@gmail.com>
2025-02-26 00:56:04 +0900

    DFS/BFS - 43162번 완료

---
+++ programmers/dfs-bfs/43162.py
@@ -0,0 +1,21 @@
+# 네트워크
+# https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+def solution(n, computers):
+    from collections import deque
+
+    result = 0
+    queue = deque()
+    indexes = [i for i in range(n)]
+
+    while indexes:
+        queue.append(indexes[0])
+        while queue:
+            m = queue.popleft()
+            indexes.remove(m)
+            for i in indexes:
+                if computers[m][i] and i not in queue:
+                    queue.append(i)
+        result += 1
+
+    return result
