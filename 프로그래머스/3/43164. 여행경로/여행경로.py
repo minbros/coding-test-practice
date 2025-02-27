55eeb0b28b0fb5585c34624738f1540765c4fc17
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-02-27 22:44:28 +0900

    [level 3] Title: 여행경로, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,28 +1,74 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+
+	
+from collections import defaultdict 
+
+def dfs(graph, N, key, footprint):
+    print(footprint)
+
+    if len(footprint) == N + 1:
+        return footprint
+
+    for idx, country in enumerate(graph[key]):
+        graph[key].pop(idx)
+
+        tmp = footprint[:]
+        tmp.append(country)
+
+        ret = dfs(graph, N, country, tmp)
+
+        graph[key].insert(idx, country)
+
+        if ret:
+            return ret
+
+
 def solution(tickets):
-    from collections import deque
-    
-    k = len(tickets)
-    queue = deque([["ICN", []]])
-    
-    while queue:
-        start, history = queue.popleft()
-        depth = len(history)
-        if depth == k:
-            result = history
-            break
+    answer = []
 
-        tmp = []
-        for i, ticket in enumerate(tickets):
-            if i not in history and ticket[0] == start:
-                new_history = history.copy()
-                new_history.append(i)
-                tmp.append([ticket[1], new_history])
+    graph = defaultdict(list)
 
-        for t in sorted(tmp):
-            queue.append(t)
-            
-    answer = ["ICN"]
-    for i in result:
-        answer.append(tickets[i][1])
-        
+    N = len(tickets)
+    for ticket in tickets:
+        graph[ticket[0]].append(ticket[1])
+        graph[ticket[0]].sort()
+
+    answer = dfs(graph, N, "ICN", ["ICN"])
+
     return answer
+
