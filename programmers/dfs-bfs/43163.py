cc2f02c6df1abc2d3fd7357bd6b8fd84356bf4c5
minbros <pristo24631@gmail.com>
2025-02-27 21:53:07 +0900

    DFS/BFS - 43163번 완료

---
+++ programmers/dfs-bfs/43163.py
@@ -0,0 +1,28 @@
+# 단어 변환
+# https://school.programmers.co.kr/learn/courses/30/lessons/43163
+
+def solution(begin, target, words):
+    from collections import deque
+
+    queue = deque([(begin, 0)])
+    k = len(words[0])
+
+    while queue:
+        word, depth = queue.popleft()
+        if word == target:
+            return depth
+
+        delete = []
+        for w in words:
+            count = 0
+            for i in range(k):
+                if word[i] != w[i]:
+                    count += 1
+            if count == 1:
+                delete.append(w)
+                queue.append((w, depth + 1))
+
+        for d in delete:
+            words.remove(d)
+
+    return 0
