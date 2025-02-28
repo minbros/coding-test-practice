4749b4e5e2a8330ae21df36f7c094f8fdd0974d7
minbros <pristo24631@gmail.com>
2025-02-28 23:44:48 +0900

    Greedy - 42861번 완료

---
+++ programmers/greedy/42861.py
@@ -0,0 +1,29 @@
+# 섬 연결하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/42861
+
+from collections import defaultdict
+import heapq
+
+
+def solution(n, costs):
+    graph = defaultdict(list)
+    for cost in costs:
+        graph[cost[0]].append((cost[1], cost[2]))
+        graph[cost[1]].append((cost[0], cost[2]))
+
+    result = 0
+    edges = [(cost, 0, end) for end, cost in graph[0]]
+    visited = [False] * n
+    visited[0] = True
+    heapq.heapify(edges)
+    while edges:
+        cost, _, end = heapq.heappop(edges)
+        if not visited[end]:
+            visited[end] = True
+            result += cost
+
+            for next_end, next_cost in graph[end]:
+                if not visited[next_end]:
+                    heapq.heappush(edges, (next_cost, end, next_end))
+
+    return result
