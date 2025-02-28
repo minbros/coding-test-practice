feb33fdc9806d8534bae3e993a5255158b9baa3f
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-03-01 00:24:20 +0900

    [level 3] Title: 섬 연결하기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,26 +1,16 @@
-from collections import defaultdict
-import heapq
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
 
 def solution(n, costs):
-    graph = defaultdict(list)
-    for cost in costs:
-        graph[cost[0]].append((cost[1], cost[2]))
-        graph[cost[1]].append((cost[0], cost[2]))
-    
-    result = 0
-    edges = [(cost, 0, end) for end, cost in graph[0]]
-    visited = [False] * n
-    visited[0] = True
-    heapq.heapify(edges)
-    while edges:
-        cost, start, end = heapq.heappop(edges)
-        if not visited[end]:
-            visited[end] = True
-            result += cost
-            
-            for next_end, next_cost in graph[end]:
-                if not visited[next_end]:
-                    heapq.heappush(edges, (next_cost, end, next_end))
-                    
-    return result
+    result = []
+    edges = sorted([(cost, start, to) for start, to, cost in costs])
+    parent = [x for x in range(n)]
+    for cost, u, v in edges:
+        if find(parent, u) != find(parent, v):
+            parent[find(parent, v)] = u
+            result.append(cost)
         
+        if len(result) == n - 1:
+            return sum(result)
