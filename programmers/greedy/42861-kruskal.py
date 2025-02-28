245d2dfed2820b26a14a5cf71c8df323877251bc
minbros <pristo24631@gmail.com>
2025-03-01 00:26:03 +0900

    Greedy - 42861번 kruskal 풀이 완료

---
+++ programmers/greedy/42861-kruskal.py
@@ -0,0 +1,21 @@
+# 섬 연결하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/42861
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def solution(n, costs):
+    result = []
+    edges = sorted([(cost, start, to) for start, to, cost in costs])
+    parent = [x for x in range(n)]
+    for cost, u, v in edges:
+        if find(parent, u) != find(parent, v):
+            parent[find(parent, v)] = u
+            result.append(cost)
+
+        if len(result) == n - 1:
+            return sum(result)
