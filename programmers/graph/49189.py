aafd446744b1fa6853f8f94a2b8c858fd0f95ba9
minbros <pristo24631@gmail.com>
2025-03-03 01:31:47 +0900

    Graph - 49189번 완료

---
+++ programmers/graph/49189.py
@@ -0,0 +1,30 @@
+# 가장 먼 노드
+# https://school.programmers.co.kr/learn/courses/30/lessons/49189
+
+from collections import defaultdict
+
+
+def solution(n, edge):
+    graph = defaultdict(list)
+    for e in edge:
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[0])
+
+    result = 0
+    starts = [1]
+    visited = [False] * (n + 1)
+    visited[0] = True
+    visited[1] = True
+    while not all(visited):
+        result = 0
+        tmp = []
+        for s in starts:
+            for v in graph[s]:
+                if not visited[v]:
+                    visited[v] = True
+                    tmp.append(v)
+                    result += 1
+
+        starts = tmp
+
+    return result
