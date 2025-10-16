9a1e8163910c65355a1ac6ea4f71e3f21add873c
minbros <pristo24631@gmail.com>
2025-10-16 23:34:24 +0900

    [Gold III] Title: 감시, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,5 +1,3 @@
-import copy
-
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 cctv = []
@@ -19,25 +17,37 @@ cases = {
     4: [((1, 0), (0, 1), (-1, 0)), ((1, 0), (0, 1), (0, -1)), ((1, 0), (-1, 0), (0, -1)), ((0, 1), (-1, 0), (0, -1))],
     5: [((1, 0), (0, 1), (-1, 0), (0, -1))]
 }
-list_directions = []
 result = 1e8
 
 
-def enable_cctv(r, c, directions, _grid):
+def enable_cctv(r, c, directions):
+    _visited = set()
     for direction in directions:
         dr, dc = direction
         nr, nc = r + dr, c + dc
-        while 0 <= nr < n and 0 <= nc < m and _grid[nr][nc] != 6:
-            if _grid[nr][nc] == 0:
-                _grid[nr][nc] = '#'
+        while 0 <= nr < n and 0 <= nc < m and grid[nr][nc] != 6:
+            if grid[nr][nc] == 0:
+                _visited.add((nr, nc))
             nr += dr
             nc += dc
 
+    return _visited
 
-def solve(count):
+
+def solve(count, visited):
     global result
     if count == len(cctv):
-        grid_copy = copy.deepcopy(grid)
-        for coord, directions in zip(cctv, list_directions):
-            r, c = coord
-            enable_cctv(r, c, directions, grid_copy)
+        cmp = n * m - len(wall) - len(cctv) - len(visited)
+        if cmp < result:
+            result = cmp
+
+        return
+
+    r, c = cctv[count]
+    for case in cases[grid[r][c]]:
+        _visited = visited | enable_cctv(r, c, case)
+        solve(count + 1, _visited)
+
+
+solve(0, set())
+print(result)
