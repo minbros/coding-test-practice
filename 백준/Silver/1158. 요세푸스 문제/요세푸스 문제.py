2e356db382fda219d2ab1e0f7b24e739daf52956
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-01 23:01:18 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 32 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,14 +1,9 @@
-from collections import deque
-
 n, k = map(int, input().split())
 answer = []
-numbers = deque(range(1, n + 1))
+numbers = list(range(1, n + 1))
+cur = 0
 while numbers:
-    x = 0
-    for _ in range(k - 1):
-        x = numbers.popleft()
-        numbers.append(x)
-    x = numbers.popleft()
-    answer.append(x)
+    cur = (cur + k - 1) % len(numbers)
+    answer.append(numbers.pop(cur))
 
 print('<' + ', '.join(map(str, answer)) + '>')
