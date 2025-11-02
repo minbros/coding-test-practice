4d8d23dea9c34bc4e9b6b669e5d643924a07cc33
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-02 22:58:26 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2752 ms, Memory: 64104 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -0,0 +1,38 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+seq = list(map(int, input().split()))
+m = int(input())
+dp = [[-1 for _ in range(n + 1)] for _ in range(n + 1)]
+
+
+def is_palindrome(start, end):
+    stack = [(start, end)]
+    while stack:
+        s, e = stack[-1]
+        if dp[s][e] != -1:
+            stack.pop()
+            continue
+
+        if s >= e:
+            dp[s][e] = 1
+            stack.pop()
+            continue
+
+        if dp[s + 1][e - 1] != -1:
+            if seq[s - 1] == seq[e - 1]:
+                dp[s][e] = dp[s + 1][e - 1]
+            else:
+                dp[s][e] = 0
+            stack.pop()
+        else:
+            stack.append((s + 1, e - 1))
+
+    return dp[start][end]
+
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(is_palindrome(s, e))
