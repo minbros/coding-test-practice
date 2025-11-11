59efc9615dba58c56312889281e00a9590c094a4
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-11 23:40:11 +0900

    [Gold III] Title: 파일 합치기, Time: 1212 ms, Memory: 43436 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -2,30 +2,27 @@ import sys
 
 input = sys.stdin.readline
 
-
-def solve(a, b):
-    if dp[a][b] != -1:
-        return dp[a][b]
-
-    if a == b:
-        return 0
-
-    result = float('inf')
-    val = sums[b + 1] - sums[a]
-    for x in range(a, b):
-        result = min(result, solve(a, x) + solve(x + 1, b) + val)
-
-    dp[a][b] = result
-    return result
-
-
 t = int(input())
 for _ in range(t):
     k = int(input())
     files = list(map(int, input().split()))
-    dp = [[-1 for _ in range(k)] for _ in range(k)]
+    dp = [[0 for _ in range(k)] for _ in range(k)]
+    a = [[0 for _ in range(k)] for _ in range(k)]
     sums = [0] * (k + 1)
+
     for i in range(1, k + 1):
         sums[i] = sums[i - 1] + files[i - 1]
 
-    print(solve(0, k - 1))
+    for L in range(2, k + 1):
+        for i in range(k - L + 1):
+            j = i + L - 1
+            preset = sums[j + 1] - sums[i]
+            start = a[i][j - 1] if a[i][j - 1] else i
+            end = a[i + 1][j] if a[i + 1][j] else j - 1
+            for x in range(start, end + 1):
+                val = dp[i][x] + dp[x + 1][j] + preset
+                if dp[i][j] > val or dp[i][j] == 0:
+                    dp[i][j] = val
+                    a[i][j] = x
+
+    print(dp[0][-1])
