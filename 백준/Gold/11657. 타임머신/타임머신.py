f525f1bcd5ee76672362f319fb065fb2aff54b2d
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-14 23:59:10 +0900

    [Gold IV] Title: 타임머신, Time: 940 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -9,16 +9,17 @@ roads = [tuple(map(int, input().split())) for _ in range(m)]
 costs = [INF for _ in range(n + 1)]
 costs[1] = 0
 
-succeeded = True
-for i in range(n):
+for i in range(n - 1):
     for now, to, cost in roads:
         if costs[to] > costs[now] + cost:
-            if i == n - 1:
-                succeeded = False
-                print(-1)
-                break
-
             costs[to] = costs[now] + cost
+            
+succeeded = True
+for now, to, cost in roads:
+    if costs[to] > costs[now] + cost:
+        succeeded = False
+        print(-1)
+        break
 
 if succeeded:
     result = [-1 if cost == INF else cost for cost in costs[2:]]
