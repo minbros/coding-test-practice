a1cf576328b1ebd58e65a40dfbabd769c8cd8894
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-17 11:34:34 +0900

    [Gold IV] Title: 운동, Time: 964 ms, Memory: 113040 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,28 @@
+import sys
+
+input = lambda: sys.stdin.readline()
+INF = float('inf')
+
+v, e = map(int, input().split())
+roads = [[INF] * v for _ in range(v)]
+for i in range(v):
+    roads[i][i] = 0
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    roads[a - 1][b - 1] = c
+
+for k in range(v):
+    for i in range(v):
+        for j in range(v):
+            roads[i][j] = min(roads[i][j], roads[i][k] + roads[k][j])
+
+answer = INF
+for i in range(v):
+    for j in range(v):
+        if i == j:
+            continue
+
+        answer = min(answer, roads[i][j] + roads[j][i])
+
+print(-1 if answer == INF else answer)
