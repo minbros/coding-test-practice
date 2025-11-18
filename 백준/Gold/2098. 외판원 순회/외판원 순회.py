95744e931f1f00696492794fbb9dcbabc5b26d57
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-18 21:30:33 +0900

    [Gold I] Title: 외판원 순회, Time: 1928 ms, Memory: 55488 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -4,13 +4,11 @@ input = lambda: sys.stdin.readline()
 
 n = int(input())
 dist = [list(map(int, input().split())) for _ in range(n)]
-dp = [[(0, float('inf'))] * n for _ in range(1 << n)]
-for i in range(n):
-    dp[1 << i][i] = (1 << i, 0)
-
+dp = [[float('inf')] * n for _ in range(1 << n)]
+dp[1][0] = 0
 for visited in range(1, 1 << n):
     for cur_node in range(n):
-        if dp[visited][cur_node][1] == float('inf'):
+        if dp[visited][cur_node] == float('inf'):
             continue
 
         if not (visited & (1 << cur_node)):
@@ -21,20 +19,14 @@ for visited in range(1, 1 << n):
                 continue
 
             new_visited = visited | (1 << next_node)
-            new_cost = dp[visited][cur_node][1] + dist[cur_node][next_node]
-            if new_cost < dp[new_visited][next_node][1]:
-                dp[new_visited][next_node] = (dp[visited][cur_node][0], new_cost)
-
-            elif new_cost == dp[new_visited][next_node][1]:
-                start_nodes = dp[visited][cur_node][0] | dp[new_visited][next_node][0]
-                dp[new_visited][next_node] = (start_nodes, new_cost)
+            new_cost = dp[visited][cur_node] + dist[cur_node][next_node]
+            dp[new_visited][next_node] = min(dp[new_visited][next_node], new_cost)
 
 result = float('inf')
-for cur_node, (next_nodes, cost) in enumerate(dp[-1]):
-    for next_node in range(n):
-        if not (next_nodes & (1 << next_node)) or dist[cur_node][next_node] == 0:
-            continue
+for node, cost in enumerate(dp[-1]):
+    if dist[node][0] == 0:
+        continue
 
-        result = min(result, cost + dist[cur_node][next_node])
+    result = min(result, cost + dist[node][0])
 
 print(result)
