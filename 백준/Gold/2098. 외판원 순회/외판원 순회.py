059ef47cc6bc14bc82aae5621987f370e9a12c1f
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2025-11-18 23:48:22 +0900

    [Gold I] Title: 외판원 순회, Time: 1768 ms, Memory: 47768 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -4,21 +4,20 @@ input = lambda: sys.stdin.readline()
 
 n = int(input())
 dist = [list(map(int, input().split())) for _ in range(n)]
-dp = [[float('inf')] * n for _ in range(1 << n)]
-dp[1][0] = 0
-for visited in range(1, 1 << n):
+dp = [[float('inf')] * n for _ in range(1 << (n - 1))]
+dp[0][0] = 0
+
+for visited in range(1 << (n - 1)):
     for cur_node in range(n):
         if dp[visited][cur_node] == float('inf'):
             continue
 
-        if not (visited & (1 << cur_node)):
-            continue
-
-        for next_node in range(n):
-            if dist[cur_node][next_node] == 0 or visited & (1 << next_node):
+        for next_node in range(1, n):
+            bit_pos = 1 << (next_node - 1)
+            if dist[cur_node][next_node] == 0 or visited & bit_pos:
                 continue
 
-            new_visited = visited | (1 << next_node)
+            new_visited = visited | bit_pos
             new_cost = dp[visited][cur_node] + dist[cur_node][next_node]
             dp[new_visited][next_node] = min(dp[new_visited][next_node], new_cost)
 
