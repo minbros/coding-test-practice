1bab2ce000e9739e22edefdb8692b4026a7c2b4a
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-01-30 19:47:49 +0900

    [Gold II] Title: 동전 분배, Time: 1020 ms, Memory: 111844 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.py
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -4,22 +4,24 @@ input = lambda: sys.stdin.readline()
 
 
 def solve():
-    dp = [[False] * (target + 1) for _ in range(n + 1)]
-    dp[0][0] = True
-    for i in range(1, n + 1):
-        _amount, _count = coins[i - 1]
-        for j in range(target + 1):
-            if not dp[i - 1][j]:
+    dp = [False] * (target + 1)
+    dp[0] = True
+    for _amount, _count in coins:
+        for i in range(target, _amount - 1, -1):
+            if not dp[i - _amount]:
                 continue
 
-            k = j
+            k = i
             iteration = 0
-            while k <= target and iteration <= _count:
-                dp[i][k] = True
+            while k <= target and iteration < _count:
+                dp[k] = True
                 k += _amount
                 iteration += 1
 
-    return dp[-1][-1]
+        if dp[-1]:
+            return True
+
+    return dp[-1]
 
 
 for _ in range(3):
