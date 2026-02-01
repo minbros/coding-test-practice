266e92f1e77d50c0ba7e7364c3b00fdc45575fcb
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-01 18:24:22 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 548 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -4,23 +4,13 @@ input = lambda: sys.stdin.readline()
 
 
 def solve():
-    dp = [(0, 0, 0)] * n
-    if n == 1:
-        return 1
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for k in (1, 2, 3):
+        for i in range(k, n + 1):
+            dp[i] += dp[i - k]
 
-    if n == 2:
-        return 2
-
-    if n == 3:
-        return 3
-
-    dp[0] = (1, 0, 0)
-    dp[1] = (1, 1, 0)
-    dp[2] = (1, 1, 1)
-    for i in range(3, n):
-        dp[i] = (dp[i - 1][0], dp[i - 2][0] + dp[i - 2][1], sum(dp[i - 3]))
-
-    return sum(dp[-1])
+    return dp[n]
 
 
 t = int(input())
