c5119a717e809a693084b71aea3524043c6c905b
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-01 19:37:22 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 38092 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,28 +1,21 @@
 import sys
 import heapq
+from collections import defaultdict
 
 input = lambda: sys.stdin.readline()
 INF = 1e8
 
-n, m, x = map(int, input().split())
-graph1 = [[INF] * (n + 1) for _ in range(n + 1)]  # 정방향 그래프
-graph2 = [[INF] * (n + 1) for _ in range(n + 1)]  # 역방향 그래프
-for _ in range(m):
-    a, b, t = map(int, input().split())
-    graph1[a][b] = t
-    graph2[b][a] = t
 
-
-def dijkstra(graph):
-    heap = [(0, x)]
+def dijkstra(graph, start):
+    heap = [(0, start)]
     dist = [0] + [INF] * n
-    dist[x] = 0
+    dist[start] = 0
     while heap:
         time, cur = heapq.heappop(heap)
         if dist[cur] < time:
             continue
 
-        for end, new_time in enumerate(graph[cur]):
+        for new_time, end in graph[cur]:
             if dist[end] > time + new_time:
                 dist[end] = time + new_time
                 heapq.heappush(heap, (time + new_time, end))
@@ -30,9 +23,17 @@ def dijkstra(graph):
     return dist
 
 
+n, m, x = map(int, input().split())
+graph1 = defaultdict(list)  # 정방향 그래프
+graph2 = defaultdict(list)  # 역방향 그래프
+for _ in range(m):
+    a, b, t = map(int, input().split())
+    graph1[a].append((t, b))
+    graph2[b].append((t, a))
+
 result = 0
-dist1 = dijkstra(graph1)
-dist2 = dijkstra(graph2)
+dist1 = dijkstra(graph1, x)
+dist2 = dijkstra(graph2, x)
 for i in range(1, n + 1):
     result = max(result, dist1[i] + dist2[i])
 
