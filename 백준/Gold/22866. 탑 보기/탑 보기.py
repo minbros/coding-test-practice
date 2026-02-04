5b8b343b217217754f97871dd9cc807e31483d58
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-04 19:27:29 +0900

    [Gold III] Title: 탑 보기, Time: 308 ms, Memory: 55380 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.py
+++ 백준/Gold/22866. 탑 보기/탑 보기.py
@@ -3,6 +3,22 @@ import sys
 input = lambda: sys.stdin.readline()
 INF = int(1e8)
 
+
+def clear_stack():
+    while stack:
+        if heights[i] < stack[-1][0]:
+            break
+
+        stack.pop()
+
+
+def renew_closest_number():
+    if stack:
+        closest = stack[-1][1]
+        if abs(closest - i) <= abs(i - closest_numbers[i] + 1):
+            closest_numbers[i] = closest + 1
+
+
 n = int(input())
 heights = list(map(int, input().split()))
 
@@ -13,36 +29,20 @@ for i in range(n - 2, -1, -1):
     if heights[i] < heights[i + 1]:
         stack.append((heights[i + 1], i + 1))
     else:
-        while stack:
-            h, k = stack[-1]
-            if heights[i] < h:
-                break
-
-            stack.pop()
+        clear_stack()
 
     counts[i] += len(stack)
-    if stack:
-        closest = stack[-1][1]
-        if abs(closest - i) < abs(i - closest_numbers[i] + 1):
-            closest_numbers[i] = closest + 1
+    renew_closest_number()
 
 stack = []
 for i in range(1, n):
     if heights[i] < heights[i - 1]:
         stack.append((heights[i - 1], i - 1))
     else:
-        while stack:
-            h, k = stack[-1]
-            if heights[i] < h:
-                break
-
-            stack.pop()
+        clear_stack()
 
     counts[i] += len(stack)
-    if stack:
-        closest = stack[-1][1]
-        if abs(closest - i) <= abs(i - closest_numbers[i] + 1):
-            closest_numbers[i] = closest + 1
+    renew_closest_number()
 
 for ans1, ans2 in zip(counts, closest_numbers):
     if ans1 == 0:
