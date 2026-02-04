fa639da8717437b70813a162337bc821cb181281
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-04 18:20:22 +0900

    [Gold V] Title: 택배 배송, Time: 288 ms, Memory: 56228 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,28 @@
+import sys
+import heapq
+from collections import defaultdict
+
+input = lambda: sys.stdin.readline()
+INF = 1e8
+
+n, m = map(int, input().split())
+graph = defaultdict(list)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+
+costs = [INF] * (n + 1)
+heap = [(0, 1)]
+while heap:
+    dist, cur = heapq.heappop(heap)
+    if costs[cur] < dist:
+        continue
+
+    for more, end in graph[cur]:
+        new_cost = dist + more
+        if costs[end] > new_cost:
+            costs[end] = new_cost
+            heapq.heappush(heap, (new_cost, end))
+
+print(costs[-1])
