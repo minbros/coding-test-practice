093a062cfb51bfabb8bca48634e619f34e87691c
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-09 20:26:49 +0900

    [Gold II] Title: 보석 도둑, Time: 1380 ms, Memory: 117492 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -4,16 +4,17 @@ import heapq
 input = lambda: sys.stdin.readline()
 
 n, k = map(int, input().split())
-jewels = [tuple(map(int, input().split())) for _ in range(n)]
-heapq.heapify(jewels)
+jewels = sorted([tuple(map(int, input().split())) for _ in range(n)])
 bags = sorted([int(input()) for _ in range(k)])
 
+idx = 0
 result = 0
 possible = []
 for bag in bags:
-    while jewels and jewels[0][0] <= bag:
-        m, v = heapq.heappop(jewels)
+    while idx < n and jewels[idx][0] <= bag:
+        m, v = jewels[idx]
         heapq.heappush(possible, (-v, m))
+        idx += 1
 
     if possible:
         result -= heapq.heappop(possible)[0]
