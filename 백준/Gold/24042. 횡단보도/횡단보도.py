1bd124b793de42099ac5ee2797ad7f6755115613
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-09 18:33:00 +0900

    [Gold I] Title: 횡단보도, Time: 3344 ms, Memory: 237744 KB -BaekjoonHub

---
+++ 백준/Gold/24042. 횡단보도/횡단보도.py
@@ -0,0 +1,30 @@
+import sys
+import heapq
+from collections import defaultdict
+
+input = lambda: sys.stdin.readline()
+INF = float('inf')
+
+n, m = map(int, input().split())
+graph = defaultdict(list)
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, i))
+    graph[b].append((a, i))
+
+heap = [(0, 1)]
+dist = [INF] * (n + 1)
+dist[1] = 0
+while heap:
+    time, cur = heapq.heappop(heap)
+    if dist[cur] < time:
+        continue
+
+    for end, idx in graph[cur]:
+        waiting = (idx - time % m + m) % m
+        new_time = time + waiting + 1
+        if dist[end] > new_time:
+            dist[end] = new_time
+            heapq.heappush(heap, (new_time, end))
+
+print(dist[-1])
