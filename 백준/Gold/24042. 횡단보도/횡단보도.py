85c2af917d76fb4692b6c02fca89013cdc012bc8
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-09 18:35:17 +0900

    [Gold I] Title: 횡단보도, Time: 2976 ms, Memory: 237204 KB -BaekjoonHub

--- 백준/Gold/24042. 횡단보도/횡단보도.py
+++ 백준/Gold/24042. 횡단보도/횡단보도.py
@@ -5,26 +5,32 @@ from collections import defaultdict
 input = lambda: sys.stdin.readline()
 INF = float('inf')
 
-n, m = map(int, input().split())
-graph = defaultdict(list)
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append((b, i))
-    graph[b].append((a, i))
 
-heap = [(0, 1)]
-dist = [INF] * (n + 1)
-dist[1] = 0
-while heap:
-    time, cur = heapq.heappop(heap)
-    if dist[cur] < time:
-        continue
+def main():
+    n, m = map(int, input().split())
+    graph = defaultdict(list)
+    for i in range(m):
+        a, b = map(int, input().split())
+        graph[a].append((b, i))
+        graph[b].append((a, i))
 
-    for end, idx in graph[cur]:
-        waiting = (idx - time % m + m) % m
-        new_time = time + waiting + 1
-        if dist[end] > new_time:
-            dist[end] = new_time
-            heapq.heappush(heap, (new_time, end))
+    heap = [(0, 1)]
+    dist = [INF] * (n + 1)
+    dist[1] = 0
+    while heap:
+        time, cur = heapq.heappop(heap)
+        if dist[cur] < time:
+            continue
 
-print(dist[-1])
+        for end, idx in graph[cur]:
+            waiting = (idx - time % m + m) % m
+            new_time = time + waiting + 1
+            if dist[end] > new_time:
+                dist[end] = new_time
+                heapq.heappush(heap, (new_time, end))
+
+    print(dist[-1])
+
+
+if __name__ == '__main__':
+    main()
