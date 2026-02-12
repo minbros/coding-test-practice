9ff412797be68ec24a5680f109ba08690294a475
Minhyung Kim <127458006+minbros@users.noreply.github.com>
2026-02-12 20:43:09 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 192 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
@@ -0,0 +1,26 @@
+import sys
+
+input = lambda: sys.stdin.readline()
+
+
+def count_stars(i1, i2, j1, j2):
+    result = 0
+    for i, j in stars:
+        if (i - i1) * (i - i2) <= 0 and (j - j1) * (j - j2) <= 0:
+            result += 1
+
+    return result
+
+
+n, m, l, k = map(int, input().split())
+stars = [tuple(map(int, input().split())) for _ in range(k)]
+answer = 0
+for star1 in stars:
+    x1 = star1[0]
+    x2 = x1 + l
+    for star2 in stars:
+        y1 = star2[1]
+        y2 = y1 + l
+        answer = max(answer, count_stars(x1, x2, y1, y2))
+
+print(k - answer)
